0717cc55ff54880fb48a8cca06d1e7ddfe57fbd4
krico <github@cwa.to>
2018-01-01 13:57:23 -0500

    More gradle

---
+++ backend/src/main/java/latebras/backend/Main.java
@@ -0,0 +1,12 @@
+package latebras.backend;
+
+/**
+ * @author krico
+ * @since 01/01/18.
+ */
+public class Main {
+
+    public static void main(String... args) {
+        System.err.println("HELLO");
+    }
+}
